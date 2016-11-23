3e2af859ab08c3f224f30df7292c36722d245be0
faxa94 <andre.chasi@gmail.com>
2016-11-23 14:48:07 -0500

    json

--- html/app.js
+++ html/app.js
@@ -1,5 +1,12 @@
-var primero = 3;
+
+
+module.exports={
+    imprimirsuma: imprimirsuma,
+}
+
+function imprimirsuma(){
+    var primero = 3;
 var segundo = 3;
 var resultado = primero + segundo;
-
 console.log(resultado);
+}
