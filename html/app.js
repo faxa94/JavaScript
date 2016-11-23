464b2e5453da93901c13174ef7f63d833f9938e8
faxa94 <andre.chasi@gmail.com>
2016-11-23 15:02:29 -0500

    guardar

--- html/app.js
+++ html/app.js
@@ -1,12 +1,4 @@
 
 
-module.exports={
-    imprimirsuma: imprimirsuma,
-}
-
-function imprimirsuma(){
-    var primero = 3;
-var segundo = 3;
-var resultado = primero + segundo;
-console.log(resultado);
-}
+var calculador= require('tecnologiaswebeadrian');
+console.log(calculadora.imprimirSuma1y2());
