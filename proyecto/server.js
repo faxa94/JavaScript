a254a91951b4a9d0d2b837504e94f9c2163ff447
faxa94 <andre.chasi@gmail.com>
2016-11-30 14:37:10 -0500

    res

--- proyecto/server.js
+++ proyecto/server.js
@@ -2,7 +2,27 @@ var e=require('express');
 var app=e();
 
 //app.use(e.static(__dirname+'/public'));
-app.get('/miservicio',function(rec,res){
-	res.send("id hola");
+var json1=[
+    {"id":1,
+     "nombre": "andres",
+     "apellido":"chasi"
+    },
+    {"id":2,
+     "nombre": "pedro",
+     "apellido":"valencia"
+    },
+    {"id":3,
+     "nombre": "pedro",
+     "apellido":"luna"
+    }
+];
+app.get('/miservicio/:id',function(rec,res){
+	var idact=rec.param.id;
+    for(var i=0;i<json1[i];i++){
+        if(idact==json1[i].id){
+            res.json(json1[i])
+        }
+    }
+    //res.send("id hola");
 });
 var server=app.listen(8090);
