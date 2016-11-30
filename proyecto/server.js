07b65914e27286c711f93676b1b15de8b60f5925
faxa94 <andre.chasi@gmail.com>
2016-11-30 14:55:03 -0500

    push

--- proyecto/server.js
+++ proyecto/server.js
@@ -2,27 +2,29 @@ var e=require('express');
 var app=e();
 
 //app.use(e.static(__dirname+'/public'));
-var json1=[
-    {"id":1,
-     "nombre": "andres",
-     "apellido":"chasi"
+var usuarios = [
+    {
+        id:1,
+        nombre:'Pepe',
+        cedula:'123409182'
     },
-    {"id":2,
-     "nombre": "pedro",
-     "apellido":"valencia"
+    {
+        id:2,
+        nombre:'Carlos',
+        cedula:'981237918'
     },
-    {"id":3,
-     "nombre": "pedro",
-     "apellido":"luna"
+    {
+        id:3,
+        nombre:'Juan',
+        cedula:'011283934'
     }
-];
-app.get('/miservicio/:id',function(rec,res){
-	var idact=rec.param.id;
-    for(var i=0;i<json1[i];i++){
-        if(idact==json1[i].id){
-            res.json(json1[i])
+]
+app.get('/miservicio/:id',function(req,res){
+	var idact=req.params.id;
+    for(var i=0;i<3;i++){
+        if(idact==usuarios[i].id){
+            res.json(usuarios[i])
         }
     }
-    //res.send("id hola");
 });
 var server=app.listen(8090);
